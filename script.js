629ed44ba4465a79281404067b58c79f6d9da9f1
emsc0001 <124031667+emsc0001@users.noreply.github.com>
2023-03-28 14:19:17 +0200

    fetch url

--- script.js
+++ script.js
@@ -3,16 +3,17 @@
 window.addEventListener("load", start);
 
 async function start() {
-  const characters = await getCharacter("data/allCharacters.json")
+  console.log(start, "App is running");
+  const characters = await getCharacter("data/allCharacters.json");
   characters.forEach(showCharacter);
 }
 
 async function getCharacter(url) {
-  const response = await fetch(url);
+  const response = await fetch("https://cederdorff.github.io/dat-js/05-data/southpark.json");
   const data = await response.json();
+  console.log(data);
   return data;
 }
-
 
 function showCharacter(character) {
   const HTML =
@@ -27,11 +28,11 @@ function showCharacter(character) {
   document.querySelector("#characters").insertAdjacentHTML("beforeend", HTML);
   document.querySelector("#characters article:last-child").addEventListener("click", characterClicked);
 
-
   function characterClicked() {
     document.querySelector("#dialog-image").src = character.image;
     document.querySelector("#dialog-name").textContent = character.name;
     document.querySelector("#dialog-nickname").textContent = character.nickname;
+
     document.querySelector("#dialog-occupation").textContent = character.occupation;
     document.querySelector("#dialog-age").textContent = character.age;
     document.querySelector("#dialog-voicedBy").textContent = character.voicedBy;
@@ -40,14 +41,13 @@ function showCharacter(character) {
     document.querySelector("#dialog-hairColor").textContent = character.hairColor;
     document.querySelector("#dialog-catchPhrase").textContent = character.catchPhrase;
     document.querySelector("#dialog-schoolGrade").textContent = character.schoolGrade;
+
     document.querySelector("#dialog-episodes").textContent = character.episodes;
     document.querySelector("#dialog-appearances").textContent = character.appearances;
     document.querySelector("#dialog-firstAppearance").textContent = character.firstAppearance;
     document.querySelector("#dialog").showModal();
   }
 }
-
-
 
 function showDialog(character) {}
 
@@ -57,27 +57,26 @@ function closeDialog() {}
 //   showCharacter(character);
 // }
 
-
 // const towelie = await getCharacter("Data/towelie.json");
-  // const jack = await getCharacter("https://raw.githubusercontent.com/YawHB/South_Park_Project/main/data/jack.json")
-  // const TolkienBlack = await getCharacter("https://raw.githubusercontent.com/Abdiox/South-park1/main/Data/southPark.json")
-  // const BarackObama = await getCharacter("https://raw.githubusercontent.com/AliHMohammad/Data-Ali/main/obamaChar.json")
-  // const Butters = await getCharacter("https://raw.githubusercontent.com/Asbjoernemil/data-assignment/main/data/characters.json")
-  // const heather = await getCharacter("https://raw.githubusercontent.com/Benjamin-Harris1/Data-app/main/data/heather.json");
-  // const Jimmy = await getCharacter("https://raw.githubusercontent.com/Forkeh/South-Park-App/main/data/jimmy.json")
-  // const Eric = await getCharacter("https://raw.githubusercontent.com/Bindholt/Data-Projekt/main/data/cartman.json")
-  // const Tuong = await getCharacter("https://raw.githubusercontent.com/TheDanishMexican/object-south-park/main/data/app.json")
-  // const Jerome = await getCharacter("https://raw.githubusercontent.com/emilvn/data-fetch-assignment/main/data/southpark.json")
-  // const Kyle = await getCharacter("https://raw.githubusercontent.com/fili0727/Data-opgave/main/kyle.json")
+// const jack = await getCharacter("https://raw.githubusercontent.com/YawHB/South_Park_Project/main/data/jack.json")
+// const TolkienBlack = await getCharacter("https://raw.githubusercontent.com/Abdiox/South-park1/main/Data/southPark.json")
+// const BarackObama = await getCharacter("https://raw.githubusercontent.com/AliHMohammad/Data-Ali/main/obamaChar.json")
+// const Butters = await getCharacter("https://raw.githubusercontent.com/Asbjoernemil/data-assignment/main/data/characters.json")
+// const heather = await getCharacter("https://raw.githubusercontent.com/Benjamin-Harris1/Data-app/main/data/heather.json");
+// const Jimmy = await getCharacter("https://raw.githubusercontent.com/Forkeh/South-Park-App/main/data/jimmy.json")
+// const Eric = await getCharacter("https://raw.githubusercontent.com/Bindholt/Data-Projekt/main/data/cartman.json")
+// const Tuong = await getCharacter("https://raw.githubusercontent.com/TheDanishMexican/object-south-park/main/data/app.json")
+// const Jerome = await getCharacter("https://raw.githubusercontent.com/emilvn/data-fetch-assignment/main/data/southpark.json")
+// const Kyle = await getCharacter("https://raw.githubusercontent.com/fili0727/Data-opgave/main/kyle.json")
 
-  // showCharacter(towelie);
-  // showCharacter(Kyle);
-  // showCharacter(Butters);
-  // showCharacter(TolkienBlack);
-  // showCharacter(Tuong);
-  // showCharacter(Jerome);
-  // showCharacter(jack);
-  // showCharacter(BarackObama);
-  // showCharacter(Jimmy);
-  // showCharacter(Eric);
-  // showCharacter(heather);
+// showCharacter(towelie);
+// showCharacter(Kyle);
+// showCharacter(Butters);
+// showCharacter(TolkienBlack);
+// showCharacter(Tuong);
+// showCharacter(Jerome);
+// showCharacter(jack);
+// showCharacter(BarackObama);
+// showCharacter(Jimmy);
+// showCharacter(Eric);
+// showCharacter(heather);
